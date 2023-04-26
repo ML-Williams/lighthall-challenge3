b4bf8ac0fe20d154007e319925f0cdf280c9e035
Mya Lookadoo-Williams <110951129+ML-Williams@users.noreply.github.com>
2023-04-26 11:39:44 -0400

    Initial commit

---
+++ src/playHangman.tsx
@@ -0,0 +1,84 @@
+import React, {useCallback} from "react";
+
+const words = [
+    'analogy',
+    'bureaucrat',
+    'catastrophe',
+    'demonstration',
+    'enthusiasm',
+    'zebra']
+
+const Hangman = () => {
+    const [word, setWord] = React.useState(words[Math.floor(Math.random() * words.length)]);
+    const [guesses, setGuesses] = React.useState('');
+    const [wrongGuesses, setWrongGuesses] = React.useState(0);
+    const [correctGuesses, setCorrectGuesses] = React.useState([]);
+
+    const handleGuess = useCallback(() => {
+            if (guesses.length === 0 ) return
+            if (!word.includes(guesses)) {
+                setWrongGuesses((wrongGuesses) => wrongGuesses + 1);
+            } else {
+                setCorrectGuesses((correctGuesses) => [...correctGuesses, guesses]);
+            }
+            setGuesses('')
+        },
+        [guesses, word]
+    );
+
+    const resetGame = useCallback(() => {
+        setWord(words[Math.floor(Math.random() * words.length)]);
+        setGuesses('');
+        setWrongGuesses(0);
+    }, []);
+
+    const isWinner = word.split("").every((letter) => guesses.includes(letter));
+
+    const hiddenWord = word.replace(/\w/g, (letter) =>
+        correctGuesses.includes(letter) ? letter : "_ "
+    );
+
+    console.log(correctGuesses);
+
+
+    const isLoser = wrongGuesses >= 6;
+
+
+    return (
+        <div>
+            <h1>Hangman</h1>
+            <p>
+                {isWinner && "You win!"}
+                {isLoser && "You lose!"}
+                {!isWinner && !isLoser && (
+                    <>
+                        {hiddenWord} <br/>
+                        Guesses: {correctGuesses.join("")} <br/>
+                        Wrong guesses: {wrongGuesses}
+                    </>
+                )}
+            </p>
+            <div>
+                {isWinner || isLoser ? (
+                    <button onClick={resetGame}>Play again</button>
+                ) : (
+                    <div>
+                        <input
+                            type="text"
+                            value={guesses}
+                            onChange={e => setGuesses(e.target.value)}
+                            maxLength={1}
+                        />
+
+                        <input
+                            type='button'
+                            onClick={handleGuess}
+                            value='Submit'
+                        />
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+};
+export default Hangman;
